4c796982bd733b84461e71d8c0bdd4a490830395
praveen jangti <praveenjm.jangti@gmail.com>
2023-04-10 23:19:54 +0200

    Igniting app

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 const parent = React.createElement('div', {id:"parent"}, [
     React.createElement('div', {className:"child1"},[
         React.createElement('h1', {}, "this is h1"), 
@@ -6,13 +8,36 @@ const parent = React.createElement('div', {id:"parent"}, [
     React.createElement('div', {className:"child2"},[
         React.createElement('h1', {}, "this is h1"), 
         React.createElement('h2', {}, "this is h2")
-    ])
+    ]) 
 ]);
+const title = <h1>React Element</h1>
+const HeaderComponent = () => {
+    // title;
+    return (
+    // <React.Fragment>
+    <>
+    <div>
+        {title}
+        <h1>This is a React Component</h1>
+        <h2>Hello</h2>
+    </div>
+    <div>
+        Other Component
+    </div>
+    <>
+    <div>Other fragment</div>
+    <div>Other fragment</div>
+    </>
+    </>
+    // </React.Fragment>
+    )
+}
+const jsxSyntax = <h1>Hello JSX</h1>
 // console.log(heading);
 // let a = heading.$$typeof;
-// console.log(typeof a)
-// console.log(typeof heading)
+// console.log(typeof a);
+// console.log(typeof heading);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 const root2 = ReactDOM.createRoot(document.getElementById('root2'));
 root.render(parent);
-root2.render(parent);
+root2.render(<HeaderComponent/>);
